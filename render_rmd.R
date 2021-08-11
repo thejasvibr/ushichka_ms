e8c017bee1c2a261147d1078b41b913921ae55a8
tbeleyur <tbeleyur@orn.mpg.de>
2021-08-11 14:01:18 +0200

    clean up messy typos and references

--- render_rmd.R
+++ render_rmd.R
@@ -1,4 +1,3 @@
-setwd('C:\\Users\\tbeleyur\\Google Drive\\Holger Goerlitz- IMPRS\\PHD_2015\\manuscripts\\Ushichka_dataset')
 files <- c('ushichka_dataset.Rmd','ushichka_dataset_supp_info.Rmd')
 print('rendering the rmd files....')
 for (f in files){rmarkdown::render(f)}
